4483d03273d2c6ad1ca54b20e41099b453c0736b
Erick Brower <erickbrower@Cynthias-MacBook-Air.local>
2017-06-21 12:11:37 -0500

    Added pages and router

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,12 @@
-console.log("Hooray!");
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import App from "./App";
+
+const app = document.getElementById('app');
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+app);
---
+++ src/js/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default class Navbar extends React.Component {
+  render() {
+    return(
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">Surveyor</a>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav">
+            <li>
+              <a href="#">Surveys</a>
+            </li>
+            <li>
+              <a href="#">Survey Results</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    );
+  }
+}
